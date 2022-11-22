b133bbabcc7ff0ed16ef5a4bb514d08782898450
Artem Goncharov <Artem.goncharov@gmail.com>
2022-11-22 16:35:01 +0100

    Update tox.ini && conf.py file (#27)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: OpenTelekomCloud Bot

--- api-ref/source/conf.py
+++ api-ref/source/conf.py
@@ -28,8 +28,6 @@ project = 'Cloud Search Service'
 otcdocs_repo_name = 'opentelekomcloud-docs/cloud-search-service'
 # Those variables are required for edit/bug links
 
-
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -93,7 +91,7 @@ html_static_path = ['_static']
 
 # -- Options for PDF output --------------------------------------------------
 latex_documents = [
-('index',
+    ('index',
      'css-api-ref.tex',
      u'Cloud Search Service - API Reference',
      u'OpenTelekomCloud', 'manual'),
