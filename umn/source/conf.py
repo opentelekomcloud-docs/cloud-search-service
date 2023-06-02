2f6723d67d369969b3409b831f7afa475fca9fa5
Tino Schr <tino.schreiber@t-systems.com>
2023-06-02 11:31:45 +0200

    Update tox.ini && conf.py file (#45)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: SebastianGode

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -28,6 +28,15 @@ project = 'Cloud Search Service'
 otcdocs_repo_name = 'opentelekomcloud-docs/cloud-search-service'
 # Those variables are required for edit/bug links
 
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = 'public'
+otcdocs_doc_link = '/cloud-search-service/umn/'
+otcdocs_doc_title = 'User Guide'
+otcdocs_doc_type = 'umn'
+otcdocs_service_category = 'big_data'
+otcdocs_service_title = 'Cloud Search Service'
+otcdocs_service_type = 'css'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
